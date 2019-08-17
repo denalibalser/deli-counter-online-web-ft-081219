669b6119c7498ea2587a87467ffa01a1e9ba153b
Denali Balser <denali.balser167@gmail.com>
2019-08-17 12:04:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length > 0
+    new_array = []
+    counter = 1
+    array.each do |name|
+      new_array << "#{counter}. #{name}"
+      counter += 1
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+   else
+    puts "The line is currently empty."
+  end
+ end
+
+ def take_a_number(array, string)
+   array.push(string)
+   puts "Welcome, #{string}. You are number #{array.length} in line."
+ end
