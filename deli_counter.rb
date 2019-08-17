37c3d842698675eb6b78cbd223cbd6f32fd15504
Denali Balser <denali.balser167@gmail.com>
2019-08-17 12:06:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,19 @@ def line(array)
    else
     puts "The line is currently empty."
   end
- end
+end
 
- def take_a_number(array, string)
+def take_a_number(array, string)
    array.push(string)
    puts "Welcome, #{string}. You are number #{array.length} in line."
- end
+end
+
+def now_serving(array)
+  if array.length > 0
+    array.each do |name|
+      puts "Currently serving #{name}."
+    end
+  else
+    puts "There is nobody in line waiting to served!"
+  end
+end 
