c26b5da5591217d4bf985e554ae89f2d12dc202e
Denali Balser <denali.balser167@gmail.com>
2019-08-17 12:07:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,6 +25,6 @@ def now_serving(array)
       puts "Currently serving #{name}."
     end
   else
-    puts "There is nobody waiting to served!"
+    puts "There is nobody waiting to be served!"
   end
 end
